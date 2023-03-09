026cf5f22c8512de6868b478b8e6695de1bfd32f
this-click <88038974+this-click@users.noreply.github.com>
2023-03-09 11:00:30 +0100

    Add project files.

---
+++ Calculator/CalculatorTests.cs
@@ -0,0 +1,14 @@
+using Domain;
+using FluentAssertions;
+
+namespace CalculatorTest
+{
+    public class CalculatorTests
+    {
+        [Fact]
+        public void Sum_of_2_and_2_should_be_4()
+        => new Calculator()
+            .Sum(2,2)
+            .Should().Be(4);
+    }
+}
---
+++ Domain/Calculator.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain
+{
+    public class Calculator
+    {
+        public int Sum(int left, int right)
+        => left + right;
+    }
+}
